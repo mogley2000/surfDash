88cd0d4eb5622bb62f17b04e148e331c20299c46
Monkey <yang.leo@gmail.com>
2020-09-23 21:59:19 +1000

    swell train scraped properly

--- scraper.py
+++ scraper.py
@@ -3,33 +3,66 @@ from bs4 import BeautifulSoup
 import logging
 import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s-  %(message)s')
+from tabulate import tabulate 
 
 # northern beaches swellnet
 
-res = requests.get('https://www.swellnet.com/reports/australia/new-south-wales/northern-beaches/forecast') 
+url = "https://www.swellnet.com/reports/australia/new-south-wales/northern-beaches/forecast"
+res = requests.get(url)
 res.raise_for_status
 soup = BeautifulSoup(res.content, 'html.parser')
-html = soup.find_all("div", {"class": "forecast_tip"}) # scrapes the swell train block of code for the whole div tag that includes class forecast_tip. will ouput 9 items (3 days x 6am, 12pm, 6pm)
+forecast = soup.find_all("div", {"class": "forecast_tip"}) # scrapes the swell train block of code for the whole div tag that includes class forecast_tip. will ouput 9 items (3 days x 6am, 12pm, 6pm)
 
-dateCond = []
-for date in html:
-    for text in date.find_all("div", {"class": "tip_date_time"}):
-        dateCond.append(text.getText())
+def getData(html, attribute, _class, index):
+    result = []
+    for tag in html:
+        for item in tag.find_all(attribute, {"class": _class})[index]:
+            if item is not None:
+                result.append(item)
+            else:
+                result.append("N/A")
+    return result
 
-waveCond = []
-for wave in html:
-    for text in wave.find_all("span", {"class": "tip_wave"}):
-        waveCond.append(text.getText())
+date = getData(forecast, "div", "tip_date_time", 0)
+train1 = getData(forecast, "div", "tip_train", 0)
+train2 = getData(forecast, "div", "tip_train", 1)
+wave = getData(forecast, "span", "tip_wave", 0)
 
-windCond = []
-for wind in html:
-    for text in wind.find_all("span", {"class": "tip_wind"}):
-        windCond.append(text.getText())
+logging.debug(date)
+logging.debug(train1)
+logging.debug(train2)
+logging.debug(wave)
+
+forecast_data = list(zip(date, train1, train2, wave))
+headers = ["Date", "Primary Swell", "Secondary Swell", "Wave Height"]
+
+print(tabulate([*forecast_data], headers=headers))
+
+#for tag in forecast:
+#    secondary = tag.find("div", {"class": "tip_train"})
+#    logging.debug(secondary) 
+    
+
+#trainCond = []
+#for train2 in forecast:
+#    for text in train2.find_all("div", {"class": "tip_train"})[1]:
+#        trainCond.append(text.getText())
+#logging.debug(trainCond)
+
+#waveCond = []
+#for wave in forecast:
+#    for text in wave.find_all("span", {"class": "tip_wave"}):
+#        waveCond.append(text.getText())
+
+#windCond = []
+#for wind in forecast:
+#    for text in wind.find_all("span", {"class": "tip_wind"}):
+#        windCond.append(text.getText())
 
 
-logging.debug(dateCond)
-logging.debug(waveCond)
-logging.debug(windCond)
+#logging.debug(dateCond)
+#logging.debug(waveCond)
+#logging.debug(windCond)
 
 #dayOne6am = html[0]
 #for tag in dayOne6am:
